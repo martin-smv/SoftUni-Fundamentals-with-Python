0eafd055ed73b0bf5cce6097b5a8b5704b398e04
martin-smv <77camperm77@gmail.com>
2023-10-09 16:31:27 +0300

    Add calculate rectangle area problem

---
+++ Fundamentals_with_Python_September_2023/labs/functions/calculate_rectangle_area.py
@@ -0,0 +1,8 @@
+# calculate_rectangle_area = lambda a, b: a * b
+def calculate_rectangle_area(a, b):
+    return a * b
+
+
+width = int(input())
+height = int(input())
+print(calculate_rectangle_area(width, height))
