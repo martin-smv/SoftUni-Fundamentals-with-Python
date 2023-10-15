2cb580a5f056a562b342073d03ba62f4c7702f60
martin-smv <77camperm77@gmail.com>
2023-10-15 17:51:04 +0300

    Updating office chairs

--- Fundamentals_with_Python_September_2023/exercises/lists_advanced/office_chairs.py
+++ Fundamentals_with_Python_September_2023/exercises/lists_advanced/office_chairs.py
@@ -1,12 +1,11 @@
 number_of_rooms = int(input())
 
 free_chairs = 0
-for current_room in range(number_of_rooms):
+for current_room in range(1, number_of_rooms + 1):
     chairs_and_visitors = input().split()
 
     number_of_chairs = len(chairs_and_visitors[0])
     number_of_visitors = int(chairs_and_visitors[1])
-    room_number = current_room + 1
 
     if number_of_chairs >= number_of_visitors:
         free_chairs += number_of_chairs - number_of_visitors
@@ -14,7 +13,7 @@ for current_room in range(number_of_rooms):
     else:
         needed_chairs = number_of_visitors - number_of_chairs
         free_chairs -= needed_chairs
-        print(f"{needed_chairs} more chairs needed in room {room_number}")
+        print(f"{needed_chairs} more chairs needed in room {current_room}")
 
 if free_chairs >= 0:
     print(f"Game On, {free_chairs} free chairs left")
