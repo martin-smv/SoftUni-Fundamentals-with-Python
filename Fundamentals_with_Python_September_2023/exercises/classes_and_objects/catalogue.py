425d6e45b6fa8358ab99a2e7a4de9c4cae655caf
martin-smv <77camperm77@gmail.com>
2023-11-02 12:20:39 +0200

    Add catalogue problem

---
+++ Fundamentals_with_Python_September_2023/exercises/classes_and_objects/catalogue.py
@@ -0,0 +1,16 @@
+class Catalogue:
+
+    def __init__(self, name: str):
+        self.name = name
+        self.products = []
+
+    def add_product(self, product_name: str):
+        self.products.append(product_name)
+
+    def get_by_letter(self, first_letter: str):
+        return [product for product in self.products if product.startswith(first_letter)]
+
+    def __repr__(self):
+        final_string = f"Items in the {self.name} catalogue:\n"
+        final_string += "\n".join(sorted(self.products))
+        return final_string
