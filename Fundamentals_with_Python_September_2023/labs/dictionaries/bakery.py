154aa89db5798745025581a940e1736218c2293b
martin-smv <77camperm77@gmail.com>
2023-11-05 14:04:17 +0200

    Add bakery problem

---
+++ Fundamentals_with_Python_September_2023/labs/dictionaries/bakery.py
@@ -0,0 +1,9 @@
+elements = input().split()
+bakery = {}
+
+for i in range(0, len(elements), 2):
+    key = elements[i]
+    quantity = int(elements[i + 1])
+    bakery[key] = quantity
+
+print(bakery)
