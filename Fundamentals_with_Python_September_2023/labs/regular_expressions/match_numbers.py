ae78920d06f4e117baaf527d5d7fb495a04ed1ac
martin-smv <77camperm77@gmail.com>
2023-11-22 14:55:45 +0200

    Add match numbers problem

---
+++ Fundamentals_with_Python_September_2023/labs/regular_expressions/match_numbers.py
@@ -0,0 +1,8 @@
+import re
+
+some_string = input()
+pattern = r"(^|(?<=\s))-?([0]|[1-9][0-9]*)(\.\d+)?($|(?=\s))"
+matches = re.finditer(pattern, some_string)
+
+for match in matches:
+    print(match.group(), end=" ")
