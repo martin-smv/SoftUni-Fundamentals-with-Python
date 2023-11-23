0214516018226df68380d4d3565b9bda9b76386b
martin-smv <77camperm77@gmail.com>
2023-11-23 12:53:37 +0200

    Add capture the numbers problem

---
+++ Fundamentals_with_Python_September_2023/exercises/regular_expressions/capture_the_numbers.py
@@ -0,0 +1,9 @@
+import re
+
+text = input()
+while text:
+    pattern = r"\d+"
+    matches = re.findall(pattern, text)
+    if matches:
+        print(" ".join(matches), end=" ")
+    text = input()
